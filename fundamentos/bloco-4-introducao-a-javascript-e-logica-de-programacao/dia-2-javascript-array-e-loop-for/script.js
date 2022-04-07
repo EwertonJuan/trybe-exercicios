293d338a15a261d818fb36275c86d41e9500a275
Ewerton Juan <juancunha2011@hotmail.com>
2022-04-07 19:21:03 -0300

    exercícios 6 ao 9 dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -40,4 +40,47 @@ for(let index3 = 0; index3 < numbers.length; index3 += 1) {
         maiorValor = numbers[index3];
     }
 }
-console.log(maiorValor);
+console.log('O maior valor é ' + maiorValor);
+
+// exercicio 6
+console.log('Exercício 6');
+
+let valoresImpares = 0;
+for(let index4 = 0; index4 < numbers.length; index4 += 1) {
+    if (numbers[index4] % 2 !== 0) {
+        valoresImpares += 1
+    }
+}
+
+if (valoresImpares > 0) {
+    console.log('Há ' + valoresImpares + ' valores ímpares');
+} else {
+    console.log('Nenhum valor ímpar encontrado');
+}
+
+// exercicio 7
+console.log('Exercício 7');
+
+let menorValor = maiorValor
+for(let index5 = 0; index5 < numbers.length; index5 += 1) {
+    if (numbers[index5] < menorValor) {
+        menorValor = numbers[index5]
+    }
+}
+console.log('O menor valor é ' + menorValor);
+
+// exercicio 8
+console.log('Exercício 8');
+
+let numbers2 = [];
+for(let index6 = 1; index6 <= 25; index6 += 1) {
+    numbers2.push(index6);
+}
+console.log(numbers2);
+
+// exercicio 9
+console.log('Exercício 9');
+
+for(let index7 = 0; index7 < numbers2.length; index7 += 1) {
+    console.log(numbers2[index7] / 2);
+}
