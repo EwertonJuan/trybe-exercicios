c9d047e1399e8364fe1594b7bb4f44921e24720e
Ewerton Juan <juancunha2011@hotmail.com>
2022-04-14 20:45:49 -0300

    Exercício 7 e falha no 8 dia 5.2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -28,3 +28,65 @@ main.appendChild(leftSection);
 let rightSection = document.createElement('section');
 rightSection.className = 'right-content'
 main.appendChild(rightSection);
+
+// exercício 7
+let image = document.createElement('img');
+image.src = 'https://picsum.photos/200';
+image.className = 'small-image';
+body[0].appendChild(image);
+
+// exercício 8
+function counter () {
+    let numbers = []
+    for (let i = 1; i < 11; i += 1) {
+        switch (i) {
+            case 1:
+                numbers.push('um')
+                break;
+            case 2:
+                numbers.push('dois')
+                break;
+            case 3:
+                numbers.push('três')
+                break;
+            case 4:
+                numbers.push('quatro')
+                break;
+            case 5:
+                numbers.push('cinco')
+                break;
+            case 6:
+                numbers.push('seis')
+                break;
+            case 7:
+                numbers.push('sete')
+                break;
+            case 8:
+                numbers.push('oito')
+                break;
+            case 9:
+                numbers.push('nove')
+                break;
+            case 10:
+                numbers.push('dez')
+                break;        
+            default:
+                break;
+        }
+    }
+    return numbers;
+}
+
+console.log(counter());
+
+// let list = createList();
+let unorderedList = document.createElement('ul');
+rightSection.appendChild(unorderedList);
+// unorderedList.appendChild(list);
+
+let listItens = document.createElement('li');
+let numbers = counter();
+for (let index = 0; index < numbers.length; index += 1) {
+    listItens.innerText = numbers[index];
+    console.log(listItens);
+}
