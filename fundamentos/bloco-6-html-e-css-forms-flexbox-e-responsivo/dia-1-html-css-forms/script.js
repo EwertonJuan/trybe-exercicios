c205daa3eb580325ef3ded5318d82535e4151b4a
Ewerton Juan <juancunha2011@hotmail.com>
2022-04-26 19:19:31 -0300

    Exercícios 3 e 4 dia 6.1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,21 @@
+const btnSubmit = document.getElementById('btn-submit');
+
+function getValues(event) {
+    event.preventDefault();
+    for (let i = 0; i < inputs.length; i += 1) {
+        console.log(inputs[i].value);
+    }
+}
+
+btnSubmit.addEventListener('click', getValues);
+
+const btnClear = document.getElementById('btn-clear');
+const inputs = document.getElementsByTagName('input');
+
+function clear() {
+    for (let i = 0; i < inputs.length; i += 1) {
+        inputs[i].value = '';
+    }
+}
+
+btnClear.addEventListener('click', clear);
