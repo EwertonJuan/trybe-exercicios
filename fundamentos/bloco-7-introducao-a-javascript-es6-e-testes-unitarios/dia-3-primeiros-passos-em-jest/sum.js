1c124eaf279581c6279b9de9152347aaa40412ed
Ewerton Juan <juancunha2011@hotmail.com>
2022-05-10 20:00:17 -0300

    Exercício 4, dia 7.3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -9,19 +9,79 @@ function sum(a, b) {
 function myRemove(arr, item) {
     let newArr = [];
     for (let index = 0; index < arr.length; index += 1) {
-      if (item !== arr[index]) {
-        newArr.push(arr[index]);
-      }
+        if (item !== arr[index]) {
+            newArr.push(arr[index]);
+        }
     }
     return newArr;
-  }
+}
 
-  function myFizzBuzz(num) {
+function myFizzBuzz(num) {
     if (typeof num !== 'number') return false;
     if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
     if (num % 3 === 0) return 'fizz';
     if (num % 5 === 0) return 'buzz';
     return num;
-  }
+}
 
-module.exports = { sum, myRemove, myFizzBuzz };
+function encode(string) {
+    let encode = '';
+    for (let index = 0; index < string.length; index += 1) {
+        switch (string[index]) {
+            case 'a':
+                encode += '1';
+                break;
+            case 'e':
+                encode += '2';
+                break;
+            case 'i':
+                encode += '3';
+                break;
+            case 'o':
+                encode += '4';
+                break;
+            case 'u':
+                encode += '5';
+                break;
+            default:
+                encode += string[index];
+                break;
+        }
+    }
+    return encode;
+}
+
+function decode(encode) {
+    let decode = '';
+    for (let index = 0; index < encode.length; index += 1) {
+        switch (encode[index]) {
+            case '1':
+                decode += 'a';
+                break;
+            case '2':
+                decode += 'e';
+                break;
+            case '3':
+                decode += 'i';
+                break;
+            case '4':
+                decode += 'o';
+                break;
+            case '5':
+                decode += 'u';
+                break;
+            default:
+                decode += encode[index];
+                break;
+        }
+    }
+    return decode;
+}
+
+module.exports = {
+    sum,
+    myRemove,
+    myFizzBuzz,
+    encode,
+    decode
+};
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,5 @@
 // Exercício 1
-const { sum, myRemove, myFizzBuzz } = require('./sum');
+const { sum, myRemove, myFizzBuzz, encode, decode } = require('./sum');
 
 describe ('Testes na função sum', () => {
     test ('Somando 4 + 5 se obtem 9', () => {
@@ -52,5 +52,29 @@ describe ('Testes na função myFizzBuzz', () => {
 
     test ('String retorna false', () => {
         expect(myFizzBuzz('15')).toBe(false);
+    });
+});
+
+// Exercício 4
+
+describe ('Testes nas funções encode e decode', () => {
+    /* test ('Verifica se encode é uma função', () => {
+        expect(encode).toBe(Function encode);
+    });
+
+    test ('Verifica se decode é uma função', () => {
+        expect(decode).toBe(Function decode);
+    }); */
+
+    test ('Verifica se vogais são substituídas', () => {
+        expect(encode('Jovem ex-quenga picha frase da Blitz')).toBe('J4v2m 2x-q52ng1 p3ch1 fr1s2 d1 Bl3tz');
+    });
+
+    test ('Verifica se números são substituídos', () => {
+        expect(decode('J4v2m 2x-q52ng1 p3ch1 fr1s2 d1 Bl3tz')).toBe('Jovem ex-quenga picha frase da Blitz');
+    });
+
+    test ('Verifica quantidade de caracteres', () => {
+        expect(encode('Jovem ex-quenga picha frase da Blitz').length).toBe(36)
     });
 });
