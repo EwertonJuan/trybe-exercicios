0d2e7b16a2e73e09f858aa2d3f496a1c8438a8fb
Ewerton Juan <juancunha2011@hotmail.com>
2022-05-13 19:19:51 -0300

    Exercícios 2 e 3 dia 8.1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,3 +1,5 @@
+// Exercício 1
+
 const createId = (name) => {
     const emailName = name.replace(/ /g, '_');
     const email = `${emailName.toLowerCase()}@trybe.com`;
@@ -10,11 +12,56 @@ const createId = (name) => {
 
 const newEmployees = (callback) => {
     const employees = {
-      id1: callback('Pedro Guerra'),
-      id2: callback('Luiza Drumond'),
-      id3: callback('Carla Paiva'),
+        id1: callback('Pedro Guerra'),
+        id2: callback('Luiza Drumond'),
+        id3: callback('Carla Paiva'),
     }
     return employees;
-  };
+};
 
-module.exports = { newEmployees, createId };
+// Exercício 2
+
+const drawnNumber = () => {
+    const drawn = Math.floor(Math.random() * 5) + 1;
+    return drawn;
+}
+
+const lotteryResult = (chosenNumber, drawnNumber) => {
+    if (chosenNumber === drawnNumber()) {
+        return 'Parabéns, você ganhou!';
+    } else {
+        return 'Tente novamente';
+    }
+}
+
+// Exercício 3
+
+const checkGrade = (gabarito, estudante) => {
+    let total = 0;
+    let grade = 0;
+    for (let i = 0; i < gabarito.length; i += 1) {
+        if (gabarito[i] === estudante[i]) {
+            total += 1;
+            grade += 1;
+        } if (gabarito[i] !== estudante[i] && estudante[i] !== 'N.A') {
+            grade -= 0.5;
+        }
+    }
+    console.log(grade);
+    return total;
+}
+
+const rightAnswers = (gabarito, estudante, callback) => {
+    const total = callback(gabarito, estudante);
+    return total;
+}
+
+// Exports
+module.exports = {
+    newEmployees,
+    createId,
+    drawnNumber,
+    lotteryResult,
+    rightAnswers,
+    checkGrade,
+};
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.test.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.test.js
@@ -1,4 +1,4 @@
-const { newEmployees, createId } = require('./script');
+const { newEmployees, createId, drawnNumber, lotteryResult, rightAnswers, checkGrade } = require('./script');
 
 describe ('Test exercises', () => {
     it('Teste exercício 1', () => {
@@ -9,5 +9,22 @@ describe ('Test exercises', () => {
                 id3: { name: 'Carla Paiva', email: 'carla_paiva@trybe.com'},
               }),
         )
+    });
+
+    it('Teste exercício 2', () => {
+        const number = drawnNumber()
+        expect(drawnNumber()).toBeGreaterThanOrEqual(1);
+        expect(drawnNumber()).toBeLessThanOrEqual(5);
+        expect(lotteryResult(number, drawnNumber)).toBe('Tente novamente');
+    });
+
+    it('Teste exercício 3', () => {
+        const answers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+        const student1 = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+        const student2 = ['A', 'N.A', 'C', 'B', 'A', 'A', 'D', 'A', 'N.A', 'C']
+        const student3 = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+        expect(rightAnswers(answers, student1, checkGrade)).toBe(6);
+        expect(rightAnswers(answers, student2, checkGrade)).toBe(6);
+        expect(rightAnswers(answers, student3, checkGrade)).toBe(10);
     });
 });
