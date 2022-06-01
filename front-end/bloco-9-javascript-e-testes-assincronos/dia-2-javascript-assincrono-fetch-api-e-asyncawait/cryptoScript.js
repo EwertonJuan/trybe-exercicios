09ef967c1aad13f9f37f3cdc717022d56294c638
Ewerton Juan <juancunha2011@hotmail.com>
2022-06-01 20:00:09 -0300

    Refatorando exercício 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/cryptoScript.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/cryptoScript.js
@@ -1,16 +1,14 @@
 const API_URL = 'https://api.coincap.io/v2/assets';
 
 const append = (name, symbol, price) => {
-    const cryptoList = document.querySelector('#crypto_list');
     const newLi = document.createElement('li');
     newLi.innerHTML = `${name} (${symbol}): ${price}`;
-    cryptoList.appendChild(newLi);
+    document.querySelector('#crypto_list').appendChild(newLi);
 };
 
 const fetchCrypto = () => {
     fetch(API_URL).then(response => response.json()).then(data => {
-        data.data.filter((coin) => coin.rank <= 10)
-        .forEach((coin) => {
+        data.data.filter((coin) => coin.rank <= 10).forEach((coin) => {
             append(coin.name, coin.symbol, coin.priceUsd);
         });
     });
